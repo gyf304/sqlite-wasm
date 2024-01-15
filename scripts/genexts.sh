b023263400be2e1d84fcdffe7e1c10039dd078f7
Yifan Gu <gyf304@gmail.com>
2024-01-14 22:39:37 -0500

    add vfsfileio

--- scripts/genexts.sh
+++ scripts/genexts.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-loads=$(grep -Eo 'sqlite3[a-zA-Z]+Init' $@ | sed 's/^/\t\t|| /g' | sed 's/$/(db)/g')
-includes=$(echo $@ | tr ' ' '\n' | sed 's/^/#include "/g' | sed 's/$/"/g' | tr '\n' ' ')
+loads=$(grep -hEo 'sqlite3[a-zA-Z]+Init' $@ | sed 's/^/\t\t|| /g' | sed 's/$/(db)/g')
+includes=$(echo $@ | tr ' ' '\n' | sed 's/^/#include "/g' | sed 's/$/"/g')
 
 cat << EOF
 $includes
--- sqlite/sqlite3exts.c
+++ sqlite/sqlite3exts.c
@@ -1,6 +1,8 @@
-#include "exts/noop.c" 
+#include "exts/noop.c"
+#include "exts/vfsfileio.c"
 
 int sqlite3_extra_autoext(sqlite3 *db) {
 	return SQLITE_OK
-		|| sqlite3NoopInit(db);
+		|| sqlite3NoopInit(db)
+		|| sqlite3VfsFileIoInit(db);
 }
